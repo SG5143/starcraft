a7ea090d68e5d491b96617e1bd0902b381c67130
SG5143 <sun5143a@gmail.com>
2024-11-08 16:11:30 +0900

    Add method getter, setter

--- src/unit/Unit.java
+++ src/unit/Unit.java
@@ -16,6 +16,25 @@ public class Unit {
 		System.out.printf("나는 %s 야\n", name);
 	}
 
+	public int getMaxHp() {
+		return this.MAXHP;
+	}
+	public int getHp() {
+		return this.hp;
+	}
+	
+	public void decreaseHp(int hp) {
+		this.hp -= hp;
+	}
+	
+	public void increaseHp(int hp) {
+		this.hp += hp;
+	}
+		
+	public String getName() {
+		return this.name;
+	}
+	
 	@Override
 	public String toString() {
 		return String.format("나는 %s이다.", name);
